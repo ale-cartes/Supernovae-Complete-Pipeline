0e0a64ef757ff7d28bf16c70e01adf486628ab50
Alejandro Cartes <alejandroml.cartes@gmail.com>
2023-03-17 14:45:24 -0300

    add peakmjd_to_days and optimize code

--- preprocess.py
+++ preprocess.py
@@ -7,27 +7,34 @@ folder = 'Supernovae-Complete-Pipeline'
 path_back = os.path.split(path)[0]
 curves_dir = os.path.join(path_back, 'curves')
 
+# read curves data
 Ia_DES_curves = reader(os.path.join(curves_dir, 'Ia', 'DES',
                                     'DES_Ia_PHOT.FITS'))
 
 nonIa_DES_curves = reader(os.path.join(curves_dir, 'nonIa', 'DES',
                                        'DES_nonIa_PHOT.FITS'))
-nonIa_summary = summary(os.path.join(path, 'Lightcurves', 'nonIa',
-                                     'DES', 'DES_nonIa.DUMP'))
 
+# .DUMP files
+Ia_dump = os.path.join(os.path.join(path, 'Lightcurves', 'Ia',
+                                    'DES', 'DES_Ia.DUMP'))
+nonIa_dump = os.path.join(os.path.join(path, 'Lightcurves', 'nonIa',
+                                       'DES', 'DES_nonIa.DUMP'))
+
+# nonIa types
+nonIa_summary = summary(nonIa_dump)
+
+# change MJD to days
+peakmjd_to_days(Ia_DES_curves, Ia_dump, inplace=True, output=False)
+peakmjd_to_days(nonIa_DES_curves, nonIa_dump, inplace=True, output=False)
+
+# add nonIa as new observations
 nonIa_DES_curves.obs += max(Ia_DES_curves.obs)
 
-mjd_to_days(Ia_DES_curves, inplace=True, output=False)
-mjd_to_days(nonIa_DES_curves, inplace=True, output=False)
+# one-hot encoder: 1 -> Ia, 0 -> nonIa
+nonIa_DES_curves['Type'] = [0] * nonIa_DES_curves.shape[0]
+Ia_DES_curves['Type'] = [1] * Ia_DES_curves.shape[0]
 
-nonIa_DES_curves['Type'] = ['nonIa'] * nonIa_DES_curves.shape[0]
-Ia_DES_curves['Type'] = ['Ia'] * Ia_DES_curves.shape[0]
-
-columns = ['obs', 'MJD', 'Days',
-           'BAND', 'FLUXCAL', 'FLUXCALERR', 'Type']
-
-curves = pd.concat((Ia_DES_curves[columns],
-                    nonIa_DES_curves[columns]))
-
+columns = ['obs', 'MJD', 'Days', 'BAND', 'FLUXCAL', 'FLUXCALERR', 'Type']
+curves = pd.concat((Ia_DES_curves[columns], nonIa_DES_curves[columns]))
 
 del Ia_DES_curves, nonIa_DES_curves  # free-up memory
--- utils.py
+++ utils.py
@@ -22,23 +22,17 @@ def reader(file_name, fits_header=False):
             header = fits_file[0].header
             print(repr(header))
 
-    light_curves = Table.read(file_name, format='fits')
-    light_curves = light_curves.to_pandas()
+    light_curves = Table.read(file_name, format='fits').to_pandas()
     index_obs_separator = light_curves[light_curves['MJD'] == -777].index
 
-    n_obs = 1
-    obs = []
-
-    for mjd in light_curves['MJD']:
-        if mjd == -777:
-            n_obs += 1
-        obs.append(n_obs)
+    obs = np.cumsum(light_curves['MJD'] == -777) + 1
 
     light_curves.insert(0, 'obs', obs)
     light_curves.drop(index_obs_separator, inplace=True)
     light_curves.reset_index(inplace=True)
     light_curves.drop(columns='index', inplace=True)
 
+    light_curves['BAND'] = light_curves.BAND.str.decode('utf-8')
     light_curves.name = file_name.split('/')[-1]
 
     return light_curves
@@ -49,14 +43,13 @@ def summary(dump_file):
     Function that reads a CSV file containing supernova
     data and sumarizes the SNTYPE column
     """
-    data = pd.read_csv(dump_file, delimiter=' ', header=5)
-    data.drop(data.columns[[0, 1]], axis=1, inplace=True)
+    data = pd.read_csv(dump_file, delimiter=' ', header=5, usecols=[11])
 
     type_map = {1: 'Ia',
                 20: 'II+IIP', 21: 'IIn+IIN', 22: 'IIL',
                 32: 'Ib', 33: 'Ic+Ibc'}
 
-    data['SNTYPE'] = data['SNTYPE'].replace(type_map)
+    data = data.replace(type_map)
 
     return data
 
@@ -95,7 +88,47 @@ def mjd_to_days(lightcurve, specific_obs=None, inplace=False, output=False):
         return days.to_numpy()
 
 
-def plotter(data_frame, obs, summary=None, days=False):
+def peakmjd_to_days(lightcurve, dump_file, specific_obs=None, inplace=False,
+                    output=False):
+    """
+    Function that transforms MJD dates to days, where day 0 corresponds to
+    the moment of the peak.
+
+    Input
+    =====
+    lightcurve: lightcurve data frame
+
+    dump_file: file that contains the moment of the peak
+
+    specific_obs: None or int (optional)
+    if it is None, days will be calculted for all data in Data Frame
+
+    inplace: bool (optional)
+    if inplace is True, the function adds a new column labeled as "Days"
+    to the dataFrame
+
+    output: bool (optional)
+    if it's True, an array of days will be returned
+    """
+
+    data = pd.read_csv(dump_file, delimiter=' ', header=5, usecols=[2, 4],
+                       names=['obs', 'PEAKMJD'])
+
+    if type(specific_obs) == int:
+        lightcurve = lightcurve[lightcurve.obs == specific_obs]
+        data = data[data.obs == specific_obs]
+
+    data = pd.merge(lightcurve, data, on='obs')
+    days = data.MJD - data.PEAKMJD
+
+    if inplace and ("Days" not in lightcurve.columns):
+        lightcurve['Days'] = days
+
+    if output:
+        return days.to_numpy()
+
+
+def plotter(data_frame, obs, summary=None, days=False, dump=None):
     """
     Function that plots supernova lightcurve
 
@@ -114,18 +147,20 @@ def plotter(data_frame, obs, summary=None, days=False):
     xlabel = 'MJD'
 
     if days:
-        mjd_to_days(data_obs, inplace=True, specific_obs=obs, output=False)
         xlabel = 'Days'
 
-    color = {'u': 'purple', 'g': 'green', 'r': 'red',
-             'i': (150/255, 0, 0), 'z': (60/255, 0, 0)}
+        # if Days have not been calculated
+        if 'Days' not in data_obs.columns:
+            peakmjd_to_days(data_obs, dump, inplace=True, specific_obs=obs,
+                            output=False)
+
+    color = {'u ': 'purple', 'g ': 'green', 'r ': 'red',
+             'i ': (150/255, 0, 0), 'z ': (60/255, 0, 0)}
 
     fig, ax = plt.subplots(figsize=(14, 8))
 
     for band in (data_obs['BAND'].value_counts()).index:
         data_to_plot = data_obs[data_obs.BAND == band]
-
-        band_str = band.decode("utf-8").replace(' ', '')
 
         xdata_plot = data_to_plot.MJD
         if days:
@@ -133,7 +168,7 @@ def plotter(data_frame, obs, summary=None, days=False):
 
         ax.errorbar(xdata_plot, data_to_plot.FLUXCAL,
                     yerr=data_to_plot.FLUXCALERR, marker='o',
-                    ls='--', capsize=2, color=color[band_str], label=band_str)
+                    ls='--', capsize=2, color=color[band], label=band)
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel('Flux (ADU)')
